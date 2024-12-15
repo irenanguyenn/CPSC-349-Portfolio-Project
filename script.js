f2271f7d261414500718f8e0bdacdffcaea71b8c
irenanguyenn <89406939+irenanguyenn@users.noreply.github.com>
2024-12-14 16:02:18 -0800

    Added confirmation alert

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function toggleMenu() {
     nav.classList.toggle('mobile');
     nav.classList.toggle('show');
 }
+function sendNotification(event) {
+    event.preventDefault(); // Prevent the form from actually submitting
+    alert("Message Sent!");
+}
